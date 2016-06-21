15bc6f5f4ee7063a3409f4d5ca5c674d6bdd2d16
CrystalHJ <1486472908@qq.com>
2016-06-21 14:55:52 +0800

    done

--- 1.js
+++ 1.js
@@ -1,3 +1,15 @@
 window.onload = function(){
+	var box = document.getElementById("box");
 	
+	box.onmousedown = function(ev){
+		var disX = ev.clientX-this.offsetLeft;
+		var disY = ev.clientY-this.offsetTop;
+		document.onmousemove = function(ev){
+			box.style.left = ev.clientX-disX+'px';
+			box.style.top = ev.clientY-disY+'px';
+		}
+		document.onmouseup = function(){
+			document.onmousemove = document.onmouseup = null;
+		}
+	}
 }
